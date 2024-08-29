716bce35f49e62b89506c4d0e068719c29396f1e
Alexandre Mahdhaoui <alexandre.mahdhaoui@gmail.com>
2024-08-29 09:31:08 +0200

    ✨ Update tmux status line
    Signed-off-by: Alexandre Mahdhaoui <alexandre.mahdhaoui@gmail.com>

---
+++ private_dot_config/tmux-powerline/segments/tmux-kube.sh
@@ -0,0 +1,9 @@
+# shellcheck shell=bash
+# Print the current date.
+
+# generate_segmentrc() {}
+
+run_segment() {
+  ${HOME}/.tmux/plugins/kube-tmux/kube.tmux 128 128 128
+	return 0
+}
--- private_dot_config/tmux-powerline/themes/my-theme.sh
+++ private_dot_config/tmux-powerline/themes/my-theme.sh
@@ -16,7 +16,7 @@ fi
 
 # See Color formatting section below for details on what colors can be used here.
 TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-'235'}
-TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:-'255'}
+TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:-'128'}
 # shellcheck disable=SC2034
 TMUX_POWERLINE_SEG_AIR_COLOR=$(air_color)
 
@@ -102,15 +102,15 @@ fi
 # shellcheck disable=SC1143,SC2128
 if [ -z "$TMUX_POWERLINE_LEFT_STATUS_SEGMENTS" ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234"
-		"hostname 33 0"
+		"tmux_session_info 235 128"
+		"hostname 236 128"
+		"lan_ip 237 128 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}"
+		"wan_ip 237 128"
+		"vcs_branch 238"
 		#"mode_indicator 165 0"
 		#"ifstat 30 255"
 		#"ifstat_sys 30 255"
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}"
 		#"vpn 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}"
-		"wan_ip 24 255"
-		"vcs_branch 29 88"
 		#"vcs_compare 60 255"
 		#"vcs_staged 64 255"
 		#"vcs_modified 9 255"
@@ -122,21 +122,22 @@ fi
 if [ -z "$TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS" ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0"
-		"pwd 89 211"
+    "tmux-kube 238 128"
+		"pwd 237 128"
+		"battery 236 128"
+		"date_day 235 128"
+		"date 235 128 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
+		"utc_time 235 128 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
 		#"macos_notification_count 29 255"
 		#"mailcount 9 255"
-		"now_playing 234 37"
+		#"now_playing 234 37"
 		#"cpu 240 136"
-		"load 237 167"
+		#load 237 167"
 		#"tmux_mem_cpu_load 234 136"
-		"battery 137 127"
 		#"air ${TMUX_POWERLINE_SEG_AIR_COLOR} 255"
-		"weather 37 255"
+		#"weather 37 255"
 		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}"
 		#"xkb_layout 125 117"
-		"date_day 235 136"
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
-		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
+		# "time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
 	)
 fi
