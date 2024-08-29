3adebecf1bde02963867090137a050a7817c4a46
Alexandre Mahdhaoui <alexandre.mahdhaoui@ionos.com>
2024-08-29 10:38:26 +0200

    🐛 Open explorer on startup if directory
    Signed-off-by: Alexandre Mahdhaoui <alexandre.mahdhaoui@ionos.com>

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -4,3 +4,20 @@ local o = vim.o
 o.cursorlineopt = "both" -- to enable cursorline!
 o.mouse = ""
 o.cmdheight = 0 -- makes lualine on top of tmux status line.
+
+local function open_nvim_tree(data)
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
+
+	if not directory then
+		return
+	end
+
+	-- change to the directory
+	vim.cmd.cd(data.file)
+
+	-- open the tree
+	require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
