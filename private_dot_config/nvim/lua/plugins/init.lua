2c831244743591feac510f0ece8e135175d0bf6b
Alexandre Mahdhaoui <alexandre.mahdhaoui@gmail.com>
2025-02-18 20:35:39 +0100

    ✨ Add support for Github Copilot
    Signed-off-by: Alexandre Mahdhaoui <alexandre.mahdhaoui@gmail.com>

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -14,6 +14,40 @@ return {
 		end,
 	},
 
+	-- Copilot (Github)
+
+	{
+		"zbirenbaum/copilot.lua",
+		lazy = false,
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({})
+		end,
+	},
+
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{
+				"zbirenbaum/copilot-cmp",
+				config = function()
+					require("copilot_cmp").setup()
+				end,
+			},
+		},
+		opts = {
+			sources = {
+				{ name = "nvim_lsp", group_index = 2 },
+				{ name = "copilot", group_index = 2 },
+				{ name = "luasnip", group_index = 2 },
+				{ name = "buffer", group_index = 2 },
+				{ name = "nvim_lua", group_index = 2 },
+				{ name = "path", group_index = 2 },
+			},
+		},
+	},
+
 	-- lspconfig
 	{
 		"neovim/nvim-lspconfig",
