014b8d8fb0022154ff856a4128a0751b89364df6
Alexandre Mahdhaoui <alexandre.mahdhaoui@gmail.com>
2025-10-05 18:18:41 +0200

    🌱 Add <leader>bcp command to neovim config
    Signed-off-by: Alexandre Mahdhaoui <alexandre.mahdhaoui@gmail.com>

--- private_dot_config/nvim/lua/custom/plugins/bufferline.lua
+++ private_dot_config/nvim/lua/custom/plugins/bufferline.lua
@@ -10,6 +10,7 @@ return {
       { "<leader>bco", "<cmd>BufferLineCloseOthers<cr>", desc = "[B]uffers: [c]lose all [o]thers" },
       { "<leader>bcl", "<cmd>BufferLineCloseLeft<cr>",   desc = "[B]uffers: [c]lose [l]eft" },
       { "<leader>bcr", "<cmd>BufferLineCloseRight<cr>",  desc = "[B]uffers: [c]lose [r]ight" },
+      { "<leader>bcp", "<cmd>BufferLinePickClose<cr>",   desc = "[B]uffers: [c]lose [p]icked" },
     },
   },
 }
